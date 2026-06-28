12eed232ef33aff218e935bd1b8652c0b437b054
Arsentiy Zaginaev <a.zaginaev@g.nsu.ru>
2026-06-28 09:35:48 +0700

    test build

--- cpp-build/main.cpp
+++ cpp-build/main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 #include <string>
 
-int main(int argc, char* argv[]) {
-    std::cout << "Hello, GitHub Actions!" << std::endl;
-    std::cout << "CMake + C++ работает отлично!" << std::endl;
-    return 0;
+int main(int argc, char* argv[]){
+    if (argc < 2){
+    std::cout << "Hello World" << std::endl;
+    }
+    else{
+        std::cout << "Hello " << argv[1] << std::endl;
+    }
 }
